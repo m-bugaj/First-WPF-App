0bcf667ec045bf6c81eb408edabbdd1074bcf4d6
bugaj55113 <bbugi2016@gmail.com>
2023-11-15 00:35:41 +0100

    Adding a pseudo-random number generator. Support for text fields, status bar and progress bar.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,5 +33,60 @@ namespace WpfApp1
             TxtBlock.Text = message;
         }
 
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textBox.Text, out int count) &&
+                int.TryParse(textBoxRangeFrom.Text, out int minValue) &&
+                int.TryParse(textBoxRangeTo.Text, out int maxValue))
+            {
+                RandomNumberGenerator generator = new RandomNumberGenerator();
+
+                // IProgress<int> progress is used to update progress
+                Progress<int> progress = new Progress<int>(value =>
+                {
+                    TxtBlock.Text = $"Postęp: {value}%";
+                    ProgrssBarApp.Value = value; // ProgressBar progress update
+                });
+
+                List<int> generatedNumbers = await generator.GenerateAsync(count, minValue, maxValue, progress);
+
+                Console.WriteLine("Wygenerowane liczby:");
+                foreach (int number in generatedNumbers)
+                {
+                    TxtOutputBlock.Text = "Wygenerowane liczby:\n" + string.Join(", ", generatedNumbers);   
+                }
+            }
+            else
+            {
+                SetStatus("Invalid input. Please enter valid numbers.");
+            }
+        }
+
+    }
+    
+    
+
+    public class RandomNumberGenerator
+    {
+        private readonly Random random = new Random();
+
+        public async Task<List<int>> GenerateAsync(int count, int minValue, int maxValue, IProgress<int> progress = null)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                // Simulation of the draw delay
+                await Task.Delay(500);
+
+                int randomNumber = random.Next(minValue, maxValue + 1);
+                result.Add(randomNumber);
+
+                // Progress update
+                progress?.Report((i + 1) * 100 / count);
+            }
+
+            return result;
+        }
     }
 }
